35b3026f92ff831019b20ef20539d1c88d4affe2
comdet <listzone@hotmail.com>
2020-01-18 15:17:34 +0700

    update git url

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
     author : 'blocks by Comdet Phueadphut',
     website : 'https://www.sensesiot.com',
     email : 'https://web.facebook.com/SensesIoT/',
-    git : 'https://github.com/cmmc-kbide/kbide-senses-weizen',
+    git : 'https://github.com/cmmc-kbide/kbide-senses-weizen/',
     image : '/static/weizen.png',
     version : '1.0.0'
 };
